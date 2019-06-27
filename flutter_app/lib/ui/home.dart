90fe0ee5991f39925ccf04bd71f82f5d587c09ae
naresh anand <arjunluam0732@gmail.com>
2019-06-27 12:57:27 +0530

    Initial Commit

---
+++ flutter_app/lib/ui/home.dart
@@ -0,0 +1,15 @@
+// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text("FirstApp"),
+      ),
+    );
+  }
+}
